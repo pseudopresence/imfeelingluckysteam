52a60a7173bc0de90c6dccffeef71ce41caca70b
Chris Swetenham <cswetenham@gmail.com>
2014-07-02 22:25:07 +0800

    Completely different version. That works.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,65 @@ using System.Text;
 using System.IO;
 using System.Diagnostics;
 
+// TODO needs fixing for current format? Compare diffs taken during and after downloads
+
+// TODO Make it possible to launch only games with a certain Category
+
+// TODO it seems a better option would be to look at all steamapps directories
+// "G:\Steam\steamapps\appmanifest_*.acf"
+// "D:\SteamLibrary\steamapps\appmanifest_*.acf"
+// If a game is installed, it will have the line "StateFlags"		"4"
+// App id is in line "appID"		"242920" (for example) and it will also be in the filename
+
 namespace SteamImFeelingLucky {
     class Program {
+        static void parseACF(StreamReader reader, List<String> ids) {
+          char[] delim = { '\t', ' ', '\"' };
+          bool is_installed = false;
+          string id = null;
+          while (!reader.EndOfStream) {
+            String line = reader.ReadLine().Trim();
+            string[] tokens = line.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+            if (tokens.GetLength(0) == 2) {
+              if (tokens[0] == "StateFlags" && tokens[1] == "4") {
+                is_installed = true;
+              }
+              if (tokens[0] == "appID") {
+                id = tokens[1];
+              }
+            }
+          }
+          if (is_installed && id != null) {
+            Console.WriteLine("Found installed game id " + id);
+            ids.Add(id);
+          }
+        }
+
         static void Main(string[] args) {
+            // TODO HAX
+            string[] steamAppsPaths = { @"G:\Steam\steamapps", @"D:\SteamLibrary\steamapps" };
+            List<String> ids = new List<String>();
+
+            foreach (string path in steamAppsPaths) {
+              string[] manifests = Directory.GetFiles(path, "appmanifest_*.acf");
+              foreach (string filename in manifests) {
+                Console.WriteLine("Opening " + filename);
+                StreamReader reader = new StreamReader(new FileStream(filename, FileMode.Open));
+                parseACF(reader, ids);
+              }
+            }
+
+            Console.Write(ids.Count + " games found");
+            Random gen = new Random();
+            int chosen = gen.Next(ids.Count);
+
+            Console.WriteLine("steam://run/" + ids[chosen]);
+            System.Diagnostics.Process.Start("steam://run/" + ids[chosen]);
+            
+            Console.WriteLine("Exiting!");
+            return;
+
+#if FALSE
             string[] filePaths = Directory.GetDirectories(SteamInstallDir() + @"\userdata\");
 
             string path = filePaths[0] + @"\config\localconfig.vdf";
@@ -18,17 +74,20 @@ namespace SteamImFeelingLucky {
             bool softwareSeen = false;
             bool appsSeen = false;
 
-            List<String> ids = new List<String>();
+            
 
+            Console.WriteLine("----------------------------------------------");
             while (!reader.EndOfStream) {
                 String line = reader.ReadLine().Trim();
 
                 switch (line) {
                     case "\"Software\"":
+                        Console.WriteLine("Found Software folder");
                         softwareSeen = true;
                         break;
                     case "\"apps\"":
                         if (softwareSeen) {
+                            Console.WriteLine("Found apps folder");
                             appsSeen = true;
                             reader.ReadLine(); //skip "{" line
                             line = reader.ReadLine();
@@ -65,29 +124,32 @@ namespace SteamImFeelingLucky {
 
                             if (check == "1") {
                                 //woohoo!
+                                Console.WriteLine("Found game id " + num);
                                 ids.Add(num);
                             }
                         } else if (line.StartsWith("\"UpdateKBtoDL\"")) {
+                            Console.WriteLine("Found UpdateKBtoDL");
                             updateKBtoDLExists = true;
                         }
                         line = reader.ReadLine().Trim(); ;
                     }
                 }
-
             }
+            Console.WriteLine("----------------------------------------------");
 
             if (ids.Count > 0) {
                 Console.Write(ids.Count + " games found");
                 Random gen = new Random();
                 int chosen = gen.Next(ids.Count);
 
-                Console.Write("steam://run/" + ids[chosen]);
+                Console.WriteLine("steam://run/" + ids[chosen]);
                 System.Diagnostics.Process.Start("steam://run/" + ids[chosen]);
             }
             else
             {
                 Console.Write("No games found!");
             }
+#endif
         }
 
         static string ProgramFilesx86() {
@@ -110,13 +172,14 @@ namespace SteamImFeelingLucky {
             } else {
                 //Steam is not running; try returning dir under program files....
                 string dumbAssumption = ProgramFilesx86() + @"\Steam";
-
+                Console.WriteLine("I have no idea so let's pretend steam install dir is: " + dumbAssumption);
                 return dumbAssumption;
             }
 
             ProcessModule steamProcessModule = steamProcess.MainModule;
             string exeDir = steamProcessModule.FileName;
             string installDir = exeDir.Substring(0, exeDir.LastIndexOf("\\")); // Steam install directory
+            Console.WriteLine("I'm pretty sure your steam install dir is: " + installDir);
             return installDir;
         }
     }
